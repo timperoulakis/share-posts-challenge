ec9f3f897ca55c267db3a84ea19a12a1863bba2e
Timothee Peroulakis <timperoulakis@gmail.com>
2022-06-23 14:36:14 +0200

    basic validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :posts
-  has_many :comments
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
+
+  validates :nickname, presence: true, length: { maximum: 50 }
 end
